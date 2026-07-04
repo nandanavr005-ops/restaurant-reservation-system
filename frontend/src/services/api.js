44da95277f111b5e8d2f9f511e923175625df87f
nandanavr005 <nandanavr005@gmail.com>
2026-07-04 16:13:28 +0530

    connected frontend with backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://restaurant-backend-7y3l.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
